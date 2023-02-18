6e056bd9e338b8d842bcac8b7ec999fb1a99a316
Kondratyeva Tatyana <t-mo_39@outlook.com>
2023-02-18 21:53:17 +0300

    Выполнила дз

---
+++ dz8/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int temp = N % 1;
+bool result = temp == 1;
+while(temp < N)
+{
+    temp = temp + 2;
+    Console.Write($"{temp} ");
+}
